c249e680951daa5669d22212462a4c16fd7733f9
XiaoHei Q <wfshenm@qq.com>
2025-09-09 22:52:02 +0800

    9.9

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,8 @@
 fn main() {
-    println!("Hello, world!");
+    let (a, mut b): (bool, bool) = (true, false);
+    // a = true,不可变; b = false，可变
+    println!("a = {:?}, b = {:?}", a, b);
+
+    b = true;
+    assert_eq!(a, b);
 }
